cfb84a7da31dae99471ac4d021aa028685d03206
pedroz2 <pedroz@umich.edu>
2020-10-20 20:35:44 -0400

    Updates

---
+++ precache-manifest.613e591c1e4f974efd9421fcd9788cc0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ebc0d954d5afce82211f2e0ee38ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "230bdff8658bb41f8f6a",
+    "url": "/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "a585480dbe5374831678",
+    "url": "/static/css/main.ec9c0095.chunk.css"
+  },
+  {
+    "revision": "230bdff8658bb41f8f6a",
+    "url": "/static/js/2.5d7b4108.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5d7b4108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a585480dbe5374831678",
+    "url": "/static/js/main.83e55aa2.chunk.js"
+  },
+  {
+    "revision": "f99cc12f948d225fb7b6",
+    "url": "/static/js/runtime-main.4938438c.js"
+  },
+  {
+    "revision": "f6753a521455302d80f81ff086486e34",
+    "url": "/static/media/pz.f6753a52.svg"
+  }
+]);
