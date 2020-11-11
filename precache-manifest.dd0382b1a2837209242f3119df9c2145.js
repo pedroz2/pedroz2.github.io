5f992bedffdb340cec87a44a69ad231724f443f2
pedroz2 <pedroz@umich.edu>
2020-11-11 00:46:22 -0500

    Updates

---
+++ precache-manifest.dd0382b1a2837209242f3119df9c2145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e5b71ae6c1ee11666639b0b3cd017c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a1434d32009f8067ed",
+    "url": "/static/css/main.a2dddaaf.chunk.css"
+  },
+  {
+    "revision": "dff5745b828a9cec4ab8",
+    "url": "/static/js/2.0d799d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d799d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a1434d32009f8067ed",
+    "url": "/static/js/main.be8510e8.chunk.js"
+  },
+  {
+    "revision": "f99cc12f948d225fb7b6",
+    "url": "/static/js/runtime-main.4938438c.js"
+  },
+  {
+    "revision": "f6753a521455302d80f81ff086486e34",
+    "url": "/static/media/pz.f6753a52.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ed1773f36f036ba6b0583364a6ac651.js"
+  "/precache-manifest.dd0382b1a2837209242f3119df9c2145.js"
 );
 
 self.addEventListener('message', (event) => {
